b6eeac7da02bd92e77d394f8d538fe7db1770114
loloxon <nikodemko@interia.pl>
2024-03-04 19:56:13 +0100

    updates

--- solver.py
+++ solver.py
@@ -11,9 +11,9 @@ def convert(file_name, topic_size, single_topics_ids):
 
 
 if __name__ == '__main__':
-    topic_size = 82
-    single_topics_ids = [1, 3, 7, 30, 31, 38, 42, 22, 25, 39, 77]
-    students_filled = 132
+    topic_size = 87
+    single_topics_ids = [1, 3, 30, 31, 42, 76, 22, 25, 39, 77]
+    students_filled = 144
 
     file_name = "input_w_pref.dzn"
     convert(file_name, topic_size, single_topics_ids)
@@ -32,8 +32,8 @@ if __name__ == '__main__':
     print("Solving time:", datetime.now() - start, "for", 100 * students_filled / 160, "|", students_filled,
           "students; avg score:", result[-1].objective / students_filled)
 
-# Solving time: 0:00: 04.501735
-# for 74.375 | 119 students; avg score: 7.65546218487395
-
-# Solving time: 0:00:03.962111
-# for 82.5 | 132 students; avg score: 7.431818181818182
+# Solving time: 0:00:04.501735 for 74.375 | 119 students; avg score: 7.65546218487395
+# Solving time: 0:00:03.962111 for 82.5 | 132 students; avg score: 7.431818181818182
+# Solving time: 0:00:03.641377 for 85.625 | 137 students; avg score: 7.452554744525547
+# Solving time: 0:00:03.531277 for 86.875 | 139 students; avg score: 7.330935251798561
+# Solving time: 0:00:03.563070 for 89.375 | 143 students; avg score: 7.384615384615385
